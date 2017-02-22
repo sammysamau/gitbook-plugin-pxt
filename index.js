dc827cd598ec509aa9bb3faa0f1aaac4ef1d0b93
Sam El <sam.elhusseini@gmail.com>
2017-02-21 21:12:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
         sim: {
             process: function(blk) {
                 var code = blk.body;
-                var base = this.book.config.getForPlugin('pxt').get('pxt', 'pxt.microbit.org');
+                var base = this.book.config.get('pxt').get('pxt', 'pxt.microbit.org');
                 var url = `https://${base}/beta---run?code=${code}`;
                 var padding = '81.97%';
                 return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
@@ -14,7 +14,7 @@ module.exports = {
         docs: {
             process: function(blk) {
                 var code = encodeURIComponent(blk.body);
-                var base = this.book.config.getForPlugin('pxt').get('pxt', 'pxt.microbit.org');
+                var base = this.book.config.get('pxt').get('pxt', 'pxt.microbit.org');
                 var url = `https://${base}/beta---docs#md:${code}:blocks:en`;
                 var padding = '70%';
                 return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
