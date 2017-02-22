014cfdaa3d325a07bdb367336bb42c9d0e166f63
Sam El <sam.elhusseini@gmail.com>
2017-02-21 16:02:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
         blocks: {
             process: function(blk) {
                 var code = encodeURIComponent('```' + blk.body + '```');
-                var url = `https://pxt.microbit.org/beta---docs?md:${code}:blocks:en`;
+                var url = `https://pxt.microbit.org/beta---docs#md:${code}:blocks:en`;
                 return '<div style="position:relative;height:0;padding-bottom:81.97%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
             }
         }
