36749210211bb39443433f723229b592f2e79afa
Sam El <sam.elhusseini@gmail.com>
2017-02-21 16:33:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ module.exports = {
             process: function(blk) {
                 var code = '```blocks\n' + blk.body + '\n```';
                 var url = `https://pxt.microbit.org/beta---docs#md:${encodeURIComponent(code)}:blocks:en`;
-                return '<div style="position:relative;height:0;padding-bottom:81.97%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
+                return '<div><iframe style="width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
             }
         },
         js: {
             process: function(blk) {
                 var code = encodeURIComponent('```blocks\n' + blk.body + '\n```');
                 var url = `https://pxt.microbit.org/beta---docs#md:${code}:js:en`;
-                return '<div style="position:relative;height:0;padding-bottom:81.97%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
+                return '<div><iframe style="width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
             }
         }
     }
