b0667d003150ca3c307e64d8e76d189b4c2f87e8
Sam El <sam.elhusseini@gmail.com>
2017-02-21 16:27:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
         blocks: {
             process: function(blk) {
                 var code = '```blocks\n' + blk.body + '\n```';
-                var url = `https://pxt.microbit.org/beta---docs#md:${code}:blocks:en`;
+                var url = `https://pxt.microbit.org/beta---docs#md:${encodeURIComponent(code)}:blocks:en`;
                 return '<div style="position:relative;height:0;padding-bottom:81.97%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
             }
         },
@@ -22,29 +22,6 @@ module.exports = {
                 var url = `https://pxt.microbit.org/beta---docs#md:${code}:js:en`;
                 return '<div style="position:relative;height:0;padding-bottom:81.97%;overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="' + url + '" allowfullscreen="allowfullscreen" sandbox="allow-popups allow-scripts allow-same-origin" frameborder="0"></iframe></div>';
             }
-        }
-    },
-
-    // Extend templating filters
-    filters: {
-        // Author will be able to write "{{ 'test'|myFilter }}"
-        myFilter: function(s) {
-            return "Hello "+s;
-        }
-    },
-
-    // Hook process during build
-    hooks: {
-        // For all the hooks, this represent the current generator
-
-        // This is called before the book is generated
-        "init": function() {
-            console.log("init!");
-        },
-
-        // This is called after the book generation
-        "finish": function() {
-            console.log("finish!");
         }
     }
 };
