2d50d01fbd5fde94a251297bdca37d5ae9084690
Sam El <sam.elhusseini@gmail.com>
2017-02-21 19:24:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = {
         sim: {
             process: function(blk) {
                 var code = blk.body;
-                var url = `https://pxt.microbit.org/beta---run?code=${code}`;
+                var base = 'pxt.microbit.org';
+                var url = `https://${base}/beta---run?code=${code}`;
                 var padding = '81.97%';
                 return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
             }
@@ -13,7 +14,8 @@ module.exports = {
         docs: {
             process: function(blk) {
                 var code = encodeURIComponent(blk.body);
-                var url = `https://pxt.microbit.org/beta---docs#md:${code}:blocks:en`;
+                var base = 'pxt.microbit.org';
+                var url = `https://${base}/beta---docs#md:${code}:blocks:en`;
                 var padding = '70%';
                 return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
             }
