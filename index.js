2358efd6c82b96415767a4ed4fab9d69711f4955
Sam El-Husseini <sam.elhusseini@gmail.com>
2017-03-06 23:20:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ module.exports = {
                 var padding = '70%';
                 return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
             }
+        },
+        code: function (blk) {
+            var lang = blk.kwargs.language;
+            var code = blk.body;
+            if (lang == "sim") {
+                var url = `https://${base}/beta---run?code=${code}`;
+                var padding = '81.97%';
+                return `<div style="position:relative;height:0;padding-bottom:${padding};overflow:hidden;"><iframe style="position:absolute;top:0;left:0;width:100%;height:100%;" src="${url}" frameborder="0" sandbox="allow-popups allow-scripts allow-same-origin"></iframe></div>`;
+            }
+            return ``;
         }
     }
 };
